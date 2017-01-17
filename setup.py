afa9257265f422311d7bf3fe82e2e317b92d5d24
Carl Cortright <ckcortright@gmail.com>
2017-01-16 22:04:46 -0700

    Added v to setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ from distutils.core import setup
 setup(
   name = 'mattermark',
   packages = ['mattermark'],
-  version = '0.1',
+  version = '0.2',
   description = 'Wrapper for the Mattermark API',
   author = 'Carl Cortright',
   author_email = 'carl@foundrygroup.com',
   url = 'https://github.com/FoundryGroup/Mattermark',
-  download_url = 'https://github.com/FoundryGroup/Mattermark/tarball/0.1',
+  download_url = 'https://github.com/FoundryGroup/Mattermark/tarball/0.2',
   keywords = ['mattermark', 'api'],
   classifiers = [],
   install_requires=[
