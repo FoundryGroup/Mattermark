3a5b50e647da8dcba4390428d1bb41f93f9e597f
Carl Cortright <ckcortright@gmail.com>
2017-01-16 22:02:23 -0700

    Added setup files

---
+++ setup.py
@@ -0,0 +1,25 @@
+################################################################################
+# Setup file for the mattermark package.
+#
+# Author: Carl Cortright
+# Date: 1/16/2017
+#
+################################################################################
+from distutils.core import setup
+setup(
+  name = 'mattermark',
+  packages = ['mattermark'],
+  version = '0.1',
+  description = 'Wrapper for the Mattermark API',
+  author = 'Carl Cortright',
+  author_email = 'carl@foundrygroup.com',
+  url = 'https://github.com/FoundryGroup/Mattermark',
+  download_url = 'https://github.com/FoundryGroup/Mattermark/tarball/0.1',
+  keywords = ['mattermark', 'api'],
+  classifiers = [],
+  install_requires=[
+        "requests",
+        "PyYAML",
+        "pprint",
+    ],
+)
